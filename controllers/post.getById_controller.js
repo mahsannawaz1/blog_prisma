423405b1b83e9099b7c6f626f0a62aa76a8e201f
Ahsan Nawaz <nawazehsen@gmail.com>
2024-07-18 15:47:15 +0500

    fixed GETALLPOSTS query

--- controllers/post.getById_controller.js
+++ controllers/post.getById_controller.js
@@ -14,30 +14,16 @@ module.exports = async (req, res) => {
     const post = await prisma.post.findUnique({
       where: {
         id: postId
+      },
+      include: {
+        comments: true,
+        likes:true
       }
     })
     if (!post) {
       return res.status(400).json({error:"Post with given ID doesn't exist"})
     } 
-    // const comments = await Comment.find({ commentedPost: post._id })
-    const comments = await prisma.comment.findMany({
-      where: {
-        commentedPost:post.id
-      }
-    })
-    // const likes = await Like.find({ likedPost: post._id })
-    const likes = await prisma.like.findMany({
-      where: {
-        likedPost:post.id
-      }
-    })
-    res.status(200).json({
-      post: { 
-        ...post,
-        comments,
-        likes
-      }
-    })
+    res.status(200).json({post})
   } catch (error) {
     console.log(error)
     res.status(500).json({error:"Internal Server/Network Error"})
